6f0070e21d0112cedc7f3ee3ade3f64e9311e411
Tawmu <tom.ferguson@outlook.com>
2020-05-21 11:05:22 +0100

    fix atatus config file

--- scripts/start-container.sh
+++ scripts/start-container.sh
@@ -281,16 +281,16 @@ fi
 ###
 if [ ! -z "$ATATUS_APM_LICENSE_KEY" ]; then
   # If API key set then configure Atatus
-  sed -i -e "s/atatus.license_key = \"\"/atatus.license_key = \"$ATATUS_APM_LICENSE_KEY\"/g" /etc/php/$PHP_VERSION/fpm/conf.d/atatus.ini
-  sed -i -e "s/atatus.release_stage = \"production\"/atatus.release_stage = \"$ENVIRONMENT\"/g" /etc/php/$PHP_VERSION/fpm/conf.d/atatus.ini
-  sed -i -e "s/atatus.app_name = \"PHP App\"/atatus.app_name = \"$SITE_NAME\"/g" /etc/php/$PHP_VERSION/fpm/conf.d/atatus.ini
-  sed -i -e "s/atatus.app_version = \"\"/atatus.app_version = \"$SITE_BRANCH-$BUILD\"/g" /etc/php/$PHP_VERSION/fpm/conf.d/atatus.ini
-  sed -i -e "s/atatus.tags = \"\"/atatus.tags = \"$SITE_BRANCH-$BUILD, $SITE_BRANCH\"/g" /etc/php/$PHP_VERSION/fpm/conf.d/atatus.ini
+  sed -i -e "s/atatus.license_key = \"\"/atatus.license_key = \"$ATATUS_APM_LICENSE_KEY\"/g" /etc/php/$PHP_VERSION/fpm/conf.d/20-atatus.ini
+  sed -i -e "s/atatus.release_stage = \"production\"/atatus.release_stage = \"$ENVIRONMENT\"/g" /etc/php/$PHP_VERSION/fpm/conf.d/20-atatus.ini
+  sed -i -e "s/atatus.app_name = \"PHP App\"/atatus.app_name = \"$SITE_NAME\"/g" /etc/php/$PHP_VERSION/fpm/conf.d/20-atatus.ini
+  sed -i -e "s/atatus.app_version = \"\"/atatus.app_version = \"$SITE_BRANCH-$BUILD\"/g" /etc/php/$PHP_VERSION/fpm/conf.d/20-atatus.ini
+  sed -i -e "s/atatus.tags = \"\"/atatus.tags = \"$SITE_BRANCH-$BUILD, $SITE_BRANCH\"/g" /etc/php/$PHP_VERSION/fpm/conf.d/20-atatus.ini
   printf " %-30s %-30s\n" "Atatus:" "Enabled"
 else
   # Atatus - if api key is not set then disable
   printf " %-30s %-30s\n" "Atatus: " "Disabled"
-  rm -f /etc/php/$PHP_VERSION/fpm/conf.d/atatus.ini
+  rm -f /etc/php/$PHP_VERSION/fpm/conf.d/20-atatus.ini
 fi
 ###
 # END Atatus configuration
